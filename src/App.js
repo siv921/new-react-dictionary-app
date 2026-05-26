c2dca6306295cf0cd0979c45bbc3e011cb6dc5f3
siv921 <asivets@me.com>
2026-05-26 11:54:13 +0100

    initial deploy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Dictionary App
+      </header>
+    </div>
+  );
+}
+
+export default App;
